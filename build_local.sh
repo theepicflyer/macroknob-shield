048c2b59b0e7a16cef02cf552af73e638e73f038
Xie Jiacheng <xiejc2000@gmail.com>
2025-01-12 23:35:03 +0800

    Basic is there

--- build_local.sh
+++ build_local.sh
@@ -9,19 +9,24 @@ cd app/
 
 west config build.cmake-args   -- "-DZMK_CONFIG=$work_dir/config"
 
-if [ -d "build/macroknob" ] && [ -d "build/settings_reset" ]; then
+if [ -d "build/macroknob_dongle" ] && [ -d "build/macroknob_lily58_left" ] && [ -d "build/macroknob_lily58_right" ] && [ -d "build/settings_reset" ]; then
     echo "Build directories exist"
-    west build -d build/macroknob
+    west build -d build/macroknob_dongle
+    west build -d build/macroknob_lily58_left
+    west build -d build/macroknob_lily58_right
     west build -d build/settings_reset
 else
     echo "No previous builds found, pristine building."
-    west build -p -d build/macroknob -b nice_nano_v2 -S studio-rpc-usb-uart -- -DSHIELD=macroknob -DZMK_EXTRA_MODULES=$work_dir -DCONFIG_ZMK_STUDIO=y
+    west build -p -d build/macroknob_dongle -b nice_nano_v2 -S studio-rpc-usb-uart -S zmk-usb-logging -- -DSHIELD=macroknob_dongle -DZMK_EXTRA_MODULES=$work_dir -DCONFIG_ZMK_STUDIO=y
+    west build -p -d build/macroknob_lily58_left -b nice_nano_v2 -S studio-rpc-usb-uart -- -DSHIELD=macroknob_lily58_left -DZMK_EXTRA_MODULES=$work_dir -DCONFIG_ZMK_STUDIO=y
+    west build -p -d build/macroknob_lily58_right -b nice_nano_v2 -S studio-rpc-usb-uart -- -DSHIELD=macroknob_lily58_right -DZMK_EXTRA_MODULES=$work_dir -DCONFIG_ZMK_STUDIO=y
     west build -p -d build/settings_reset -b nice_nano_v2 -- -DSHIELD=settings_reset -DZMK_EXTRA_MODULES=$work_dir
-
 fi
 
 output_dir=$work_dir/uf2
 mkdir -p $output_dir
 
-cp -v build/macroknob/zephyr/zmk.uf2 $output_dir/macroknob.uf2
-cp -v build/settings_reset/zephyr/zmk.uf2 $output_dir/macroknob-settings-reset.uf2
+cp -v build/macroknob_dongle/zephyr/zmk.uf2 $output_dir/macroknob_dongle.uf2
+cp -v build/macroknob_lily58_left/zephyr/zmk.uf2 $output_dir/macroknob_lily58_left.uf2
+cp -v build/macroknob_lily58_right/zephyr/zmk.uf2 $output_dir/macroknob_lily58_right.uf2
+cp -v build/settings_reset/zephyr/zmk.uf2 $output_dir/macroknob_settings_reset.uf2
